74dc1b688cea592e9eabdb2b5b1c07b80b3681fe
Alex Yang <42564458+ayang98@users.noreply.github.com>
2018-10-30 16:26:15 -0500

    Update matrix_2D.h

--- matrix_2D.h
+++ matrix_2D.h
@@ -14,7 +14,7 @@ typedef struct {
 
 } Matrix;
 
-Matrix matrix_constuctor(int n1, int n2, char *tag);
+Matrix matrix_constructor(int n1, int n2, char *tag);
 
 void destructor();
 
@@ -24,7 +24,7 @@ double matrix_get(Matrix m, int row, int column);
 
 Matrix matrix_add(Matrix m1, Matrix m2, char *tag);
 
-Matrix matrix_products(Matrix m1, Matrix m2, char *tag);
+Matrix matrix_product(Matrix m1, Matrix m2, char *tag);
 
 Matrix matrix_read(char *filename, char *tag);
 
